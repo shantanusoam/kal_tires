cb0ad1771702bfb40bcdc68990a4a51a765df764
shantanusoam <51102185+shantanusoam@users.noreply.github.com>
2022-08-08 23:34:00 +0530

    change in icon

--- Components/upperbar/TopUpbar.jsx
+++ Components/upperbar/TopUpbar.jsx
@@ -1,5 +1,28 @@
 import React from 'react';
+import Link from 'next/link';
+import Image from 'next/image';
 
-const TopUpbar = () => <div className="tire-gray-3">Navbbar</div>;
+const TopUpbar = () => (
+  <div className="bg-tire-gray-3 flexBetween w-full z-10  p-4 flex-row">
+    <Link href="/">
+      <div>
+        <div>Indianapolis, IN</div>
+        <div>
+          <Image
+            src="/../../Assets/Icons/Place.png"
+            objectFit="contain"
+            // width={12}
+            // height={12}
+            alt="place"
+            layout="fill"
+          />
+        </div>
+      </div>
+    </Link>
+    <Link href="/">
+      <div>Help</div>
+    </Link>
+  </div>
+);
 
 export default TopUpbar;
--- pages/_app.js
+++ pages/_app.js
@@ -1,16 +1,19 @@
-import Script from 'next/script';
-import { Navbar, Footer } from '../Components';
+// import Script from 'next/script';
+import { Navbar, Footer, TopUpbar, Topbar } from '../Components';
+
 import '../styles/globals.css';
 
 const MyApp = ({ Component, pageProps }) => (
   <div>
+    <TopUpbar />
+    <Topbar />
     <Navbar />
     <Component {...pageProps} />
     <Footer />
-    <Script
+    {/* <Script
       src="https://kit.fontawesome.com/d16bf6a891.js"
       crossOrigin="anonymous"
-    />
+    /> */}
   </div>
 );
 
