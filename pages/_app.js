68483d1aa03ad719169d0ba70b948c8f51995069
shantanusoam <51102185+shantanusoam@users.noreply.github.com>
2022-08-08 21:23:03 +0530

    changes in navbar and footer component

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,13 @@
+import Footer from '../Components/Footer';
+import Navbar from '../Components/Navbar';
 import '../styles/globals.css';
 
-const MyApp = ({ Component, pageProps }) => <Component {...pageProps} />;
+const MyApp = ({ Component, pageProps }) => (
+  <div>
+    <Navbar />
+    <Component {...pageProps} />
+    <Footer />
+  </div>
+);
 
 export default MyApp;
