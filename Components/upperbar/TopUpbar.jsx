f3b5e3b53d039606487d7f409be70d156ea00990
shantanusoam <51102185+shantanusoam@users.noreply.github.com>
2022-08-09 00:43:14 +0530

    change

--- Components/upperbar/TopUpbar.jsx
+++ Components/upperbar/TopUpbar.jsx
@@ -1,27 +1,32 @@
 import React from 'react';
 import Link from 'next/link';
-import Image from 'next/image';
+// import Image from 'next/image';
+
+import { GoLocation } from 'react-icons/go';
 
 const TopUpbar = () => (
-  <div className="bg-tire-gray-3 flexBetween w-full z-10  p-4 flex-row">
-    <Link href="/">
-      <div>
-        <div>Indianapolis, IN</div>
-        <div>
-          <Image
+  <div className="bg-tire-gray-3 ">
+    <div className="flexBetween w-full z-10 mx-8 p-4 flex-row">
+      <Link href="/">
+        <div className="flex flex-row flexCenter">
+          <div className="text-white font-bold">Indianapolis, IN</div>
+          <div className="ml-2 font-bold">
+            <GoLocation color="white " />
+            {/* <Image
             src="/../../Assets/Icons/Place.png"
             objectFit="contain"
             // width={12}
             // height={12}
             alt="place"
             layout="fill"
-          />
+          /> */}
+          </div>
         </div>
-      </div>
-    </Link>
-    <Link href="/">
-      <div>Help</div>
-    </Link>
+      </Link>
+      <Link href="/">
+        <div className="text-white">Help</div>
+      </Link>
+    </div>
   </div>
 );
 
