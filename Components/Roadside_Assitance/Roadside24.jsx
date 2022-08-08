3ccd37ad031e34d251f494240856089059f4dd6d
Kd <umarkhurshid3@gmail.com>
2022-08-09 04:29:21 +0530

    roadside assistance update

--- Components/Roadside_Assitance/Roadside24.jsx
+++ Components/Roadside_Assitance/Roadside24.jsx
@@ -1,35 +1,97 @@
-import React from 'react';
+import Image from 'next/image';
+import { IoIosCheckmark } from 'react-icons/io';
+import { MdLocalPhone } from 'react-icons/md';
+import { BiMessageDetail } from 'react-icons/bi';
+import servicetruck from '../../Assets/Images/Roadside_Assistance/servicetruck.png';
 
-export const Roadside24 = () => (
-  <div id="maincontainer">
-    <div id="innermaincontainer">
-      <div className="bg-indigo-600">
+const Roadside = () => (
+  <div id="roadmain">
+    <div id="roadmaininner">
+      <div className="bg-bg-white-1">
         <div className="lg:grid lg:grid-cols-2">
-          <div className="py-24 px-10 lg:px-0 max-w-3xl lg:max-w-md mx-auto">
-            <h2 className="text-4xl tracking-tight font-extrabold text-gray-100">
-              <span className="block">Ready to dive in?</span>
-              <span className="block">Start your free trial today.</span>
+          <div className="lg:relative lg:mt-32">
+            <Image
+              className="lg:absolute lg:inset-0 h-60 w-full lg:h-full object-cover object-center lg:rounded-tl-md  "
+              src={servicetruck}
+              alt="servicetruck"
+            />
+          </div>
+
+          <div className="py-12 md:px-10 px-6 lg:px-0 max-w-3xl lg:max-w-md mx-auto">
+            <h2 className="text-3xl tracking-tight  text-black font-poppins font-semibold">
+              <span className="block">
+                <span className="text-kaltire-red">24 / 7</span> ROADSIDE
+                ASSITANCE
+              </span>
             </h2>
-            <p className="text-gray-300 mt-5">
+            <p className="text-black mt-5">
               Lorem Ipsum is simply dummy text of the printing and typesetting
-              industry. Lorem Ipsum has been the industry &#39s standard dummy
+              industry. Lorem Ipsum has been the industry&#39;s standard dummy
               text ever since the 1500s, when an unknown printer took a galley
               of type and scrambled it to make a type specimen book.
             </p>
-            <div className="inline-block shadow mt-5">
-              <a
-                href="/"
-                className="inline-block py-3 px-4 bg-white hover:bg-indigo-100 text-indigo-500 font-medium border border-transparent rounded-md"
-              >
-                Sign up for free
-              </a>
+            <div className="inline-block py-3">
+              <p className="pt-2">
+                <div className="flex items-center ">
+                  <span>
+                    <IoIosCheckmark className="h-12 w-12 text-kaltire-red" />
+                  </span>
+                  <span>Lorem ipsum dolor sit.</span>
+                </div>
+              </p>
+              <p className="pt-2">
+                <div className="flex items-center ">
+                  <span>
+                    <IoIosCheckmark className="h-12 w-12 text-kaltire-red" />
+                  </span>
+                  <span>Lorem ipsum dolor sit.</span>
+                </div>
+              </p>
+              <p className="pt-2">
+                <div className="flex items-center ">
+                  <span>
+                    <IoIosCheckmark className="h-12 w-12 text-kaltire-red" />
+                  </span>
+                  <span>Lorem ipsum dolor sit.</span>
+                </div>
+              </p>
+              <p className="pt-2">
+                <div className="flex items-center ">
+                  <span>
+                    <IoIosCheckmark className="h-12 w-12 text-kaltire-red" />
+                  </span>
+                  <span>Lorem ipsum dolor sit.</span>
+                </div>
+              </p>
+            </div>
+            {/* The Phone and Email icons start  */}
+            <div className="block pt-4" id="PhoneEmailRoad" />
+            <div id="InnerphoneEmailRoad" className="flex">
+              <div id="Hotline" className="flex items-center">
+                <div id="Hotline-first" className="md:mx-4 mx-2">
+                  <MdLocalPhone className="h-6 w-6 text-kaltire-red" />
+                </div>
+                <div id="Hotline-second">
+                  <h1 className="font-bold">Hot Line</h1>
+                  <p>800-808-0025</p>
+                </div>
+              </div>
+              <div id="LiveChat" className="md:ml-6 ml-10 flex items-center">
+                <div id="LivechatFirst" className="md:mx-4 mx-2">
+                  <BiMessageDetail className="h-6 w-6 text-kaltire-red" />
+                </div>
+                <div id="LivechatSecond">
+                  <h1 className="font-bold">Live Chat </h1>
+                  <p>Chat with experts</p>
+                </div>
+              </div>
             </div>
           </div>
-          <div className="lg:relative lg:mt-16">
-            {/* <img class="lg:absolute lg:inset-0 h-60 w-full lg:h-full object-cover object-center lg:rounded-tl-md" src="https://images.pexels.com/photos/2559941/pexels-photo-2559941.jpeg?auto=compress&cs=tinysrgb&dpr=3&h=750&w=1260" alt="Woman workcation on the beach"> */}
-          </div>
+
+          {/* The Phone and Email icons end */}
         </div>
       </div>
     </div>
   </div>
 );
+export default Roadside;
---
+++ pages/RoadsideAssistance.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import Roadside from '../Components/Roadside_Assitance/Roadside24';
+
+const RoadsideAssistance = () => (
+  <>
+    {' '}
+    <Roadside />
+  </>
+);
+
+export default RoadsideAssistance;
