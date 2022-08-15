7a6bcfd131f748b50b2bc7ca0bf71c2bfba32cd4
Kd <umarkhurshid3@gmail.com>
2022-08-16 03:49:59 +0530

    commercialtire comp update

--- Components/What_We_Offer/CommercialTire.jsx
+++ Components/What_We_Offer/CommercialTire.jsx
@@ -1,67 +1,81 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Image from 'next/image';
-import blacktire from '../../Assets/Images/Whatweoffer/speciality/blacktire.png';
 
-const CommercialTire = () => (
-  <div id="commercialtiremain">
-    <div id="commertiremaininner">
-      <div id="specialmain">
-        <div
-          id="specialmaininner"
-          style={{
-            backgroundImage:
-              // eslint-disable-next-line operator-linebreak
-              'url(' +
-              'https://raw.githubusercontent.com/shantanusoam/kal_tires/kaltireBranch/Assets/Images/Whatweoffer/speciality/cooperbackg.png' +
-              ')',
-            backgroundRepeat: 'no-repeat',
-            backgroundSize: 'cover',
-          }}
-        >
-          <div className="bg-speciality-background">
-            <div className="lg:flex  items-center">
-              {/* <div className="lg:hidden block">
-                <div className="lg:relative lg:mt-4 self-start ">
-                  <Image
-                    className="lg:absolute lg:inset-0 h-60 w-full lg:h-full object-cover object-center lg:rounded-tl-md  "
-                    src={blacktire}
-                    alt="tires"
-                  />
+import { CommercialTireone, CommercialTiretwo } from '../../data/data';
+
+// const thedata = [CommercialTireone[0].logos];
+// console.log(thedata);
+// console.log(alldata);
+const CommercialTire = () => {
+  const [alldata, setalldata] = useState(CommercialTireone[0]);
+  console.log(alldata);
+  return (
+    <div id="commercialtiremain">
+      <div id="commertiremaininner">
+        <div id="commercemain">
+          <div id="commercemaininner">
+            <div className="bg-black">
+              <div className="lg:grid lg:grid-cols-3 ">
+                <div className="lg:block ">
+                  <div className="lg:relative self-start ">
+                    <Image
+                      className="lg:absolute lg:inset-0 h-60 w-full lg:h-full object-cover object-center lg:rounded-tl-md "
+                      src={alldata.mainimage}
+                      alt="tires"
+                    />
+                  </div>
                 </div>
-              </div> */}
-              <div className="lg:block hidden bg-kaltire-red">
-                <div className="lg:relative lg:mt-4 float-right ml-56">
-                  <Image
-                    className="lg:absolute lg:inset-0 h-60 w-full lg:h-full object-cover object-center lg:rounded-tl-md"
-                    src={blacktire}
-                    alt="tires"
-                  />
+                {/* <div className="lg:hidden block">
+        <div className="lg:relative lg:mt-4 self-start ">
+          <Image
+            className="lg:absolute lg:inset-0 h-60 w-full lg:h-full object-cover object-center lg:rounded-tl-md  "
+            src={blacktire}
+            alt="tires"
+          />
+        </div>
+      </div> */}
+                <div className="lg:py-2 md:px-10 px-6 lg:px-0 max-w-5xl  mx-auto col-span-2  text-center lg:text-left mt-16 ">
+                  <h1 className="text-white font-poppins text-4xl font-bold">
+                    {alldata.Heading}
+                  </h1>
+                  <div id="combuttons" className="mt-12">
+                    <span className="">
+                      <button
+                        className="   mr-2 text-center focus:border-b-2 border-white text-white  transition-all duration-100 delay-75 cursor-pointer    focus:text-kaltire-red"
+                        onClick={() => setalldata(CommercialTireone[0])}
+                      >
+                        {alldata.titleone}
+                      </button>
+                    </span>
+                    &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+                    <span className="">
+                      <button
+                        className="mr-2 text-center focus:border-b-2 border-white text-white  transition-all duration-100 delay-75 cursor-pointer    focus:text-kaltire-red "
+                        onClick={() => setalldata(CommercialTiretwo[0])}
+                      >
+                        {alldata.titletwo}
+                      </button>
+                    </span>
+                  </div>
+                  <p className="text-white mt-5 ">{alldata.desc}</p>
+                  <div className="flex items-center">
+                    {alldata.logos.map((indexnew) => (
+                      <div
+                        id="commlogos"
+                        className="mt-8 ml-4"
+                        key={indexnew.id}
+                      >
+                        <Image src={indexnew.img} className="flex" />
+                      </div>
+                    ))}
+                  </div>
                 </div>
-              </div>
-              <div className="lg:py-12 md:px-10 px-6 lg:px-0 max-w-5xl  mx-auto col-span-2  text-center lg:text-left flex-2">
-                <p className="text-black mt-5">
-                  Lorem ipsum dolor sit amet consectetur, adipisicing elit.
-                  Voluptatibus architecto facere corporis vitae, voluptatem
-                  optio molestiae neque omnis doloribus inventore nulla aliquid.
-                  Ipsum velit excepturi corporis esse autem blanditiis
-                  veritatis! Veniam officia, autem ducimus vitae ex ea.
-                  Possimus, magnam, autem repellat inventore voluptate sit
-                  tempore soluta cupiditate explicabo quod quos similique
-                  debitis temporibus. Maiores officia vitae culpa eum porro
-                  fugit cupiditate aliquam, dignissimos adipisci minima
-                  voluptate eos enim cum animi, totam assumenda. Commodi quaerat
-                  consequuntur maxime, rem quis, enim, velit provident
-                  asperiores voluptatem voluptate esse reiciendis vitae ipsa
-                  hic. Dolor excepturi et veniam! Quaerat omnis optio reiciendis
-                  asperiores soluta dolorem?
-                </p>
               </div>
             </div>
           </div>
         </div>
       </div>
     </div>
-  </div>
-);
-
+  );
+};
 export default CommercialTire;
